d50315a4326785576876bd466949e9d6b88b4afd
zehrei verj morales <moraleszehreiverj8@gmail.com>
2025-06-11 19:06:52 +0800

    feat: add about me

---
+++ src/app/about-me/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { TypographyH1 } from '@/components/ui/typography';
+
+export default function AboutMe() {
+    return (
+        <div className="size-full">
+            <TypographyH1 className="mt-20 mb-40 text-center">About Me</TypographyH1>
+        </div>
+    );
+}
