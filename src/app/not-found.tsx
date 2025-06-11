46a321c947fddd6f5350195b74cc2515e58ec6c1
zehrei verj morales <moraleszehreiverj8@gmail.com>
2025-06-11 16:30:50 +0800

    feat: add not-found

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function NotFound() {
+    return (
+        <div className="flex size-full items-center justify-center">
+            <h1 className="text-4xl font-extrabold text-gray-500/50">Page not found</h1>
+        </div>
+    );
+}
