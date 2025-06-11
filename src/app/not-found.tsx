59604634d734b08c12788e0a62420b10e6fe4bfc
zehrei verj morales <moraleszehreiverj8@gmail.com>
2025-06-11 18:47:06 +0800

    fix: set not-found height

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,7 +4,7 @@ import { TypographyH1 } from '@/components/ui/typography';
 
 export default function NotFound() {
     return (
-        <div className="flex size-full items-center justify-center">
+        <div className="flex h-[calc(100vh-100px)] items-center justify-center">
             <TypographyH1 className="-translate-y-10 text-gray-500/50">Page not found</TypographyH1>
         </div>
     );
