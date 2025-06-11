0e174453ea8b109c40b46e04a79e15060f909c9d
zehrei verj morales <moraleszehreiverj8@gmail.com>
2025-06-11 19:14:40 +0800

    feat: dynamic social links

--- src/components/layouts/partials/socials.tsx
+++ src/components/layouts/partials/socials.tsx
@@ -2,23 +2,35 @@
 
 import Link from 'next/link';
 import React from 'react';
+import { IconType } from 'react-icons';
 import { FaLinkedin, FaSquareGithub } from 'react-icons/fa6';
 
 import { Button } from '@/components/ui/button';
 
+interface ILink {
+    href: string;
+    icon: IconType;
+}
+
+const LINKS = [
+    { href: 'https://www.linkedin.com/in/zehrei-verj-morales', icon: FaLinkedin },
+    { href: 'https://github.com/ZVDCM', icon: FaSquareGithub },
+] as const satisfies ILink[];
+
 export default function Socials() {
     return (
         <div className="flex gap-1 justify-self-end">
-            <Link href="#">
-                <Button variant="ghost" size="icon" className="cursor-pointer">
-                    <FaLinkedin />
-                </Button>
-            </Link>
-            <Link href="#">
-                <Button variant="ghost" size="icon" className="cursor-pointer">
-                    <FaSquareGithub />
-                </Button>
-            </Link>
+            {LINKS.map((link, index) => {
+                const Icon = link.icon;
+
+                return (
+                    <Link href={link.href} key={index}>
+                        <Button variant="ghost" size="icon" className="cursor-pointer">
+                            <Icon />
+                        </Button>
+                    </Link>
+                );
+            })}
         </div>
     );
 }
