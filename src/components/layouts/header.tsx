4d0400a1ae2b15aa18781388e00243ff4747b3bb
zehrei verj morales <moraleszehreiverj8@gmail.com>
2025-06-12 22:14:33 +0800

    feat: disable projects route

--- src/components/layouts/header.tsx
+++ src/components/layouts/header.tsx
@@ -3,13 +3,12 @@
 import React from 'react';
 
 import Logo from '@/components/layouts/partials/logo';
-import NavBar from '@/components/layouts/partials/nav-bar';
 
 export default function Header() {
     return (
         <header className="sticky top-0 z-[100] container mx-auto flex h-[100px] items-center justify-between px-8 py-4">
             <Logo />
-            <NavBar />
+            {/* <NavBar /> */}
         </header>
     );
 }
