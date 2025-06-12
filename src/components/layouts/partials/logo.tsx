c19b16155752069bfe7f78993f386d0cc563f403
zehrei verj morales <moraleszehreiverj8@gmail.com>
2025-06-12 22:26:29 +0800

    feat: add passHref

--- src/components/layouts/partials/logo.tsx
+++ src/components/layouts/partials/logo.tsx
@@ -8,7 +8,7 @@ import { cn } from '@/lib/utils/cn';
 
 export default function Logo({ className, ...rest }: React.ComponentProps<'small'>) {
     return (
-        <Link href="/" className="w-fit text-white/50 transition-colors hover:text-white">
+        <Link passHref href="/" className="w-fit text-white/50 transition-colors hover:text-white">
             <TypographySmall className={cn('font-extralight', className)} {...rest}>
                 ZVM
             </TypographySmall>
