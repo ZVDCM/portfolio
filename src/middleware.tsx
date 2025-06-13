f12a395b1cfe649c2cea6ca5a9ec69fc0995896d
zehrei verj morales <moraleszehreiverj8@gmail.com>
2025-06-13 23:29:37 +0800

    feat: add matcher

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -36,3 +36,16 @@ export function middleware(request: NextRequest) {
 
     return response;
 }
+
+export const config = {
+    matcher: [
+        /*
+         * Match all request paths except for the ones starting with:
+         * - api (API routes)
+         * - _next/static (static files)
+         * - _next/image (image optimization files)
+         * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+         */
+        '/((?!api|_next/static|_next/image|/files|favicon.ico|sitemap.xml|robots.txt).*)',
+    ],
+};
