6fcea27a9c2191f5047e2f8fd66d79a8889b47a6
zehrei verj morales <moraleszehreiverj8@gmail.com>
2025-06-13 09:42:05 +0800

    feat: add employments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Employments from '@/components/pages/about-me/employments';
 import Hero from '@/components/pages/about-me/hero';
 import Technologies from '@/components/pages/about-me/technologies';
 
@@ -6,6 +7,7 @@ export default function AboutMe() {
         <div className="flex size-full flex-col gap-10">
             <Hero />
             <Technologies />
+            <Employments />
         </div>
     );
 }
---
+++ src/components/pages/about-me/employments.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import React from 'react';
+
+export default function Employments() {
+    return (
+        <section className="container mx-auto flex items-center justify-center px-8">
+            <article className="mt-10 w-[800px]"></article>
+        </section>
+    );
+}
