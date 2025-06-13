054d0b584eeaac16b3ea705fa90dc16a25854fd5
zehrei verj morales <moraleszehreiverj8@gmail.com>
2025-06-13 22:23:06 +0800

    style: add container

--- src/components/layouts/aside.tsx
+++ src/components/layouts/aside.tsx
@@ -7,9 +7,11 @@ import Socials from '@/components/layouts/partials/socials';
 
 export default function Aside() {
     return (
-        <aside className="fixed bottom-8 flex w-full items-center justify-between px-8">
-            <DownloadResume />
-            <Socials />
+        <aside className="fixed bottom-8 w-full">
+            <div className="container mx-auto flex items-center justify-between px-8">
+                <DownloadResume />
+                <Socials />
+            </div>
         </aside>
     );
 }
